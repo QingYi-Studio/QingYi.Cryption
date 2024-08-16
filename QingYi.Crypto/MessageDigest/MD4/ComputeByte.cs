bc24ee3b7189fa3ed3c5739b1420963793d444ca
Grey Wind <wjunqin7@gmail.com>
2024-08-16 22:15:33 +0800

    md4

--- QingYi.Crypto/MessageDigest/MD4/ComputeByte.cs
+++ QingYi.Crypto/MessageDigest/MD4/ComputeByte.cs
@@ -13,7 +13,7 @@
         /// 返回包含十六进制散列的字符串
         /// </summary>
         /// <param name = "b">byte-array to input<br></br>要输入的字节数组</param>
-        /// <returns>String that contains the hex of the hash<br></br>包含哈希十六进制的字符串</returns>
+        /// <returns>String that contains the hex of the hash|包含哈希十六进制的字符串</returns>
         public static string GetHexHashFromBytes(byte[] b)
         {
             return md4.GetHexHashFromBytes(b);
@@ -24,10 +24,21 @@
         /// 从输入字节返回字节哈希值
         /// </summary>
         /// <param name = "b">byte to hash<br></br>字节到散列</param>
-        /// <returns>binary hash of the input byte<br></br>输入字节的二进制哈希值</returns>
+        /// <returns>binary hash of the input byte|输入字节的二进制哈希值</returns>
         public static byte[] GetByteHashFromByte(byte b)
         {
             return md4.GetByteHashFromByte(b);
+        }
+
+        /// <summary>
+        /// Returns a string that contains the hexadecimal hash<br></br>
+        /// 返回包含十六进制散列的字符串
+        /// </summary>
+        /// <param name = "b">byte to hash<br></br>字节到散列</param>
+        /// <returns>String that contains the hex of the hash|包含哈希十六进制的字符串</returns>
+        public static string GetHexHashFromByte(byte b)
+        {
+            return md4.GetHexHashFromByte(b);
         }
     }
 }
