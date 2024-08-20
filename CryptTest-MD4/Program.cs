ff78a7e06f29c8ef1e5ab545acbf36091b6fc62c
Grey Wind <wjunqin7@gmail.com>
2024-08-20 22:28:37 +0800

    delete test code

--- CryptTest-MD4/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using CryptTest_MD4;
-
-MD4Old old = new MD4Old();
-
-Console.WriteLine(old.GetHexHashFromString("Hello, world"));
--- CryptTest-TripleDes/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using CryptTest_TripleDes;
-
-TDesString desString = new TDesString("123456781234567812345678", "12345678");
-string encrypt = desString.ECBEncrypt("HaHaHa");
-string decrypt = desString.ECBDecrypt(encrypt);
-
-Console.WriteLine(encrypt);
-Console.WriteLine(decrypt);
