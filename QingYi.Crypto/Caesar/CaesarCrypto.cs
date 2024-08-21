5b09f51a63727e3729ca11cf0e787fdf51298cee
Grey Wind <wjunqin7@gmail.com>
2024-08-22 01:17:13 +0800

    2.3.0 beta 1 (#11)
    * init
    
    * sm2 c source code
    
    * Delete SM2.rar
    
    * Update QingYi.Crypto.csproj
    
    * optimize xor code
    
    * add comments
    
    * 合并旧凯撒代码
    
    * Update QingYi.Crypto.csproj

--- QingYi.Crypto/Caesar/CaesarCrypto.cs
+++ QingYi.Crypto/Caesar/CaesarCrypto.cs
@@ -2,7 +2,11 @@
 
 namespace Caesar
 {
-    public class CaesarTextCryption
+    /// <summary>
+    /// Carsar crypto.<br></br>
+    /// Carsar加密。
+    /// </summary>
+    public class CaesarCrypto
     {
         /// <summary>
         /// Encrypts the specified text using Caesar cipher. Caesar cipher encrypts the text by shifting each character's Unicode value by a specified amount.<br></br>
@@ -17,36 +21,9 @@ namespace Caesar
         /// 用于加密的位移量。可以是正整数（向右偏移）或负整数（向左偏移）。
         /// </param>
         /// <returns>
-        /// The encrypted string. Each character in the string is shifted by the specified amount.<br></br>
-        /// 加密后的字符串。该字符串中的每个字符都被移位了指定的位移量。
+        /// The encrypted string. Each character in the string is shifted by the specified amount.|加密后的字符串。该字符串中的每个字符都被移位了指定的位移量。
         /// </returns>
-        public static string Encrypt(string text, int shift)
-        {
-            return CaesarEncrypt(text, shift);
-        }
-
-        /// <summary>
-        /// Decrypts the specified encrypted text using Caesar cipher. Caesar decryption shifts each character's Unicode value in the reverse direction by a specified amount to decrypt the text.<br></br>
-        /// 对指定的加密文本进行凯撒解密。凯撒解密通过将每个字符的Unicode值按指定的位移量进行反向偏移来解密文本。
-        /// </summary>
-        /// <param name="text">
-        /// The encrypted string to be decrypted. It can contain any Unicode characters, such as English, Chinese, special symbols, etc.<br></br>
-        /// 要解密的加密字符串。可以包含任何Unicode字符，如英文、中文、特殊符号等。
-        /// </param>
-        /// <param name="shift">
-        /// The amount of shift used for decryption. It should be the same as the shift amount used for encryption.<br></br>
-        /// 用于解密的位移量。应与加密时使用的位移量相同。
-        /// </param>
-        /// <returns>
-        /// The decrypted string. Each character in the string is shifted by the specified amount to restore the original text.<br></br>
-        /// 解密后的字符串。该字符串中的每个字符都被移位了指定的位移量，恢复到原始文本。
-        /// </returns>
-        public static string Decrypt(string text, int shift)
-        {
-            return CaesarDecrypt(text, shift);
-        }
-
-        private static string CaesarEncrypt(string text, int shift)
+        public static string EncryptString(string text, int shift)
         {
             // 创建一个StringBuilder对象，用于存储加密后的字符
             StringBuilder result = new StringBuilder();
@@ -62,7 +39,22 @@ namespace Caesar
             return result.ToString();
         }
 
-        private static string CaesarDecrypt(string text, int shift)
+        /// <summary>
+        /// Decrypts the specified encrypted text using Caesar cipher. Caesar decryption shifts each character's Unicode value in the reverse direction by a specified amount to decrypt the text.<br></br>
+        /// 对指定的加密文本进行凯撒解密。凯撒解密通过将每个字符的Unicode值按指定的位移量进行反向偏移来解密文本。
+        /// </summary>
+        /// <param name="text">
+        /// The encrypted string to be decrypted. It can contain any Unicode characters, such as English, Chinese, special symbols, etc.<br></br>
+        /// 要解密的加密字符串。可以包含任何Unicode字符，如英文、中文、特殊符号等。
+        /// </param>
+        /// <param name="shift">
+        /// The amount of shift used for decryption. It should be the same as the shift amount used for encryption.<br></br>
+        /// 用于解密的位移量。应与加密时使用的位移量相同。
+        /// </param>
+        /// <returns>
+        /// The decrypted string. Each character in the string is shifted by the specified amount to restore the original text.|解密后的字符串。该字符串中的每个字符都被移位了指定的位移量，恢复到原始文本。
+        /// </returns>
+        public static string DecryptString(string text, int shift)
         {
             // 创建一个StringBuilder对象，用于存储解密后的字符
             StringBuilder result = new StringBuilder();
---
+++ QingYi.Crypto/XOR/XORCrypto.cs
@@ -0,0 +1,176 @@
+﻿using System.Text;
+using System;
+
+namespace XOR
+{
+    /// <summary>
+    /// XOR crypto.<br></br>
+    /// XOR加密。
+    /// </summary>
+    public class XorCrypto
+    {
+        /// <summary>
+        /// Encodes a plain text string using a specified key with XOR encryption and returns the encoded string in Base64 format.<br></br>
+        /// 使用带有异或加密的指定密钥对纯文本字符串进行编码，并以Base64格式返回编码后的字符串。
+        /// </summary>
+        /// <param name="plainText">The plain text string to be encoded.<br></br>要编码的纯文本字符串。</param>
+        /// <param name="key">The key used for XOR encryption.<br></br>用于异或加密的密钥。</param>
+        /// <returns>A Base64 encoded string representing the XOR encrypted plain text.|Base64编码的字符串，表示异或加密的纯文本。</returns>
+        public static string EncodeString(string plainText, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] plainTextArray = Encoding.ASCII.GetBytes(plainText);
+            byte[] cypherTextArray = new byte[plainTextArray.Length];
+
+            for (int i = 0; i < plainTextArray.Length; i++)
+            {
+                cypherTextArray[i] = (byte)(plainTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return Convert.ToBase64String(cypherTextArray);
+        }
+
+        /// <summary>
+        /// Decodes a Base64 encoded XOR encrypted string back to its original plain text using the specified key.<br></br>
+        /// 使用指定的密钥将Base64编码的XOR加密字符串解码回其原始纯文本。
+        /// </summary>
+        /// <param name="cypherText">The Base64 encoded XOR encrypted string to be decoded.<br></br>要解码的Base64编码的异或加密字符串。</param>
+        /// <param name="key">The key used for XOR decryption.<br></br>用于异或解密的密钥。</param>
+        /// <returns>The original plain text string after decoding.|解码后的原始纯文本字符串。</returns>
+        public static string DecodeString(string cypherText, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] cypherTextArray = Convert.FromBase64String(cypherText);
+            byte[] plainTextArray = new byte[cypherTextArray.Length];
+
+            for (int i = 0; i < cypherTextArray.Length; i++)
+            {
+                plainTextArray[i] = (byte)(cypherTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return Encoding.UTF8.GetString(plainTextArray);
+        }
+
+        /// <summary>
+        /// Encodes a plain text string to a byte array using XOR encryption with the specified key.<br></br>
+        /// 使用使用指定密钥的异或加密将纯文本字符串编码为字节数组。
+        /// </summary>
+        /// <param name="plainText">The plain text string to be encoded.<br></br>要编码的纯文本字符串。</param>
+        /// <param name="key">The key used for XOR encryption.<br></br>用于异或加密的密钥。</param>
+        /// <returns>A byte array representing the XOR encrypted plain text.|表示异或加密纯文本的字节数组。</returns>
+        public static byte[] EncodeStringToBytes(string plainText, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] plainTextArray = Encoding.ASCII.GetBytes(plainText);
+            byte[] cypherTextArray = new byte[plainTextArray.Length];
+
+            for (int i = 0; i < plainTextArray.Length; i++)
+            {
+                cypherTextArray[i] = (byte)(plainTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return cypherTextArray;
+        }
+
+        /// <summary>
+        /// Decodes a Base64 encoded XOR encrypted byte array back to its original plain text using the specified key.<br></br>
+        /// 使用指定的密钥将Base64编码的XOR加密字节数组解码回其原始纯文本。
+        /// </summary>
+        /// <param name="cypherText">The Base64 encoded XOR encrypted string to be decoded.<br></br>要解码的Base64编码的异或加密字符串。</param>
+        /// <param name="key">The key used for XOR decryption.<br></br>用于异或解密的密钥。</param>
+        /// <returns>A byte array representing the original plain text.|表示原始纯文本的字节数组。</returns>
+        public static byte[] DecodeStringToBytes(string cypherText, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] cypherTextArray = Convert.FromBase64String(cypherText);
+            byte[] plainTextArray = new byte[cypherTextArray.Length];
+
+            for (int i = 0; i < cypherTextArray.Length; i++)
+            {
+                plainTextArray[i] = (byte)(cypherTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return plainTextArray;
+        }
+
+        /// <summary>
+        /// Encodes a byte array to a Base64 string using XOR encryption with the specified key.<br></br>
+        /// 使用使用指定密钥的异或加密将字节数组编码为Base64字符串。
+        /// </summary>
+        /// <param name="plainTextArray">The byte array to be encoded.<br></br>要编码的字节数组。</param>
+        /// <param name="key">The key used for XOR encryption.<br></br>用于异或加密的密钥。</param>
+        /// <returns>A Base64 encoded string representing the XOR encrypted byte array.|表示异或加密字节数组的Base64编码字符串。</returns>
+        public static string EncodeBytesToString(byte[] plainTextArray, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] cypherTextArray = new byte[plainTextArray.Length];
+
+            for (int i = 0; i < plainTextArray.Length; i++)
+            {
+                cypherTextArray[i] = (byte)(plainTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return Convert.ToBase64String(cypherTextArray);
+        }
+
+        /// <summary>
+        /// Decodes a Base64 encoded XOR encrypted string back to a byte array using the specified key.<br></br>
+        /// 使用指定的密钥将Base64编码的XOR加密字符串解码回字节数组。
+        /// </summary>
+        /// <param name="cypherTextArray">The Base64 encoded XOR encrypted byte array to be decoded.<br></br>要解码的Base64编码的异或加密字节数组。</param>
+        /// <param name="key">The key used for XOR decryption.<br></br>用于异或解密的密钥。</param>
+        /// <returns>A byte array representing the original plain text.|表示原始纯文本的字节数组。</returns>
+        public static string DecodeBytesToString(byte[] cypherTextArray, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] plainTextArray = new byte[cypherTextArray.Length];
+
+            for (int i = 0; i < cypherTextArray.Length; i++)
+            {
+                plainTextArray[i] = (byte)(cypherTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return Encoding.UTF8.GetString(plainTextArray);
+        }
+
+        /// <summary>
+        /// Encodes a byte array using XOR encryption with the specified key.<br></br>
+        /// 使用指定密钥使用异或加密对字节数组进行编码。
+        /// </summary>
+        /// <param name="plainTextArray">The byte array to be encoded.<br></br>要编码的字节数组。</param>
+        /// <param name="key">The key used for XOR encryption.<br></br>用于异或加密的密钥。</param>
+        /// <returns>A byte array representing the XOR encrypted byte array.|表示异或加密字节数组的字节数组。</returns>
+        public static byte[] EncodeBytes(byte[] plainTextArray, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] cypherTextArray = new byte[plainTextArray.Length];
+
+            for (int i = 0; i < plainTextArray.Length; i++)
+            {
+                cypherTextArray[i] = (byte)(plainTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return cypherTextArray;
+        }
+
+        /// <summary>
+        /// Decodes an XOR encrypted byte array back to its original form using the specified key.<br></br>
+        /// 使用指定的密钥将XOR加密的字节数组解码回其原始形式。
+        /// </summary>
+        /// <param name="cypherTextArray">The XOR encrypted byte array to be decoded.<br></br>要解码的异或加密字节数组。</param>
+        /// <param name="key">The key used for XOR decryption.<br></br>用于异或解密的密钥。</param>
+        /// <returns>A byte array representing the original plain text.|表示原始纯文本的字节数组。</returns>
+        public static byte[] DecodeBytes(byte[] cypherTextArray, string key)
+        {
+            byte[] keyArray = Encoding.ASCII.GetBytes(key);
+            byte[] plainTextArray = new byte[cypherTextArray.Length];
+
+            for (int i = 0; i < cypherTextArray.Length; i++)
+            {
+                plainTextArray[i] = (byte)(cypherTextArray[i] ^ keyArray[i % keyArray.Length]);
+            }
+
+            return plainTextArray;
+        }
+    }
+}
