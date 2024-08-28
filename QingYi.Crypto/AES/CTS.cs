97fcce1199f1bee8bfb088d675e8f34cd7b6da15
Grey Wind <wjunqin7@gmail.com>
2024-08-29 01:35:40 +0800

    2.4.0 (#17)
    * alpha 1
    
    * update release notes
    
    * update readme
    
    * PKCS7
    
    * update release note
    
    * Complete the XML comments
    
    * complete cbc mode
    
    * fix readme error
    
    * Update QingYi.Crypto.csproj
    
    * Update QingYi.Crypto.csproj
    
    * aes mode diff doc
    
    * Delete PKCS7.cs
    
    * ecb mode
    
    * CFB mode
    
    * Update QingYi.Crypto.csproj
    
    * why nobody use cts or ofb
    
    * Update QingYi.Crypto.csproj

---
+++ QingYi.Crypto/AES/CTS.cs
@@ -0,0 +1,10 @@
+﻿namespace AES
+{
+    internal class CTS
+    {
+        // example code notfound
+        // why nobody use ofb and cts mode?
+        // there's no official documentation from microsoft
+        // fuck
+    }
+}
